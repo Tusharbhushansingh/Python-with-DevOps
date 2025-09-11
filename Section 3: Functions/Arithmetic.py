7bb7e98297220059b84c924119973680285fc065
Tushar Kumar <42302678+Tusharbhushansingh@users.noreply.github.com>
2025-09-11 17:03:17 +0000

    function

---
+++ Section 3: Functions/Arithmetic.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def muliply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
+
+a = 10
+b = 7
+
+print(f"Additon of {a} + {b} = {add(a,b)}")
+print(f"Subtraction of {a} - {b} = {subtract(a,b)}")
+print(f"Multiplication of {a} * {b} = {muliply(a,b)}")
+print(f"Division of {a} + {b} = {divide(a,b)}")
---
+++ Section 3: Functions/NamedFun.py
@@ -0,0 +1,15 @@
+def greet(name):
+    print(f"Hello ! {name}, Welcome to Pyhton tutorial")
+
+
+greet('Charlie')
+greet('Bob')
+greet('Alice')
+
+def add_numbers(a,b):
+    return a + b
+
+a = 5
+b = 6
+
+print(f"Additon of {a} + {b} = {add_numbers(a,b)}")
